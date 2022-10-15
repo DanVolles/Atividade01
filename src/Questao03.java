a36119479777ec85fd5765da0e2d11e15ecd277e
Danilo <danilo.volles@outlook.com>
2022-10-15 19:54:19 -0300

    Resolução da questão 3.

--- src/Questao03.java
+++ src/Questao03.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class Questao03 {
+    public static void main(String[] args) {
+
+        System.out.println("Olá, muito bem vindo!\n" +
+                "Por favor, digite o seu nome: ");
+
+        Scanner input = new Scanner(System.in);
+        String nome = input.nextLine();
+
+        System.out.println("Olá, " + nome + "!\n" +
+                "Tenha um excelente dia!");
+    }
 }
